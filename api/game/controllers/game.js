878344198c9645da18112ae63850ec3f0adda8b7
Felipe Borges <lipex360@gmail.com>
2022-03-22 20:27:54 -0300

    populating games initialize

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,11 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  populate: (ctx) => {
+    console.log('Starting to populate')
 
-module.exports = {};
+    console.log(ctx.query)
+
+    ctx.send("Finished populating!")
+  }
+};
