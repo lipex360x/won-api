e294d8d86a5537275d36a1ca3d7e06a05b2d8e49
Felipe Borges <lipex360@gmail.com>
2022-03-22 21:02:15 -0300

    get data dom with jsdom

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -1,12 +1,29 @@
 'use strict';
 const axios = require('axios')
 
+async function getGameInfo(slug) {
+  const jsdom = require("jsdom")
+  const { JSDOM } = jsdom
+
+  const body = await axios.get(`https://www.gog.com/en/game/${slug}`)
+
+  const dom = new JSDOM(body.data)
+
+  const description = dom.window.document.querySelector('.description')
+
+  return {
+    rating: 'BR0',
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML
+  }
+}
+
 module.exports = {
   populate: async (params) => {
     const gogUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
 
     const { data: { products }} = await axios.get(gogUrl)
 
-    console.log(products[0])
+    console.log(await getGameInfo(products[0].slug))
   }
 };
